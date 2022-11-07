1db7f6bb6767413374f56a33b7f42ed5c0e3dce3
Maciej Cywinski <cyvvilek@gmail.com>
2022-11-07 19:55:41 +0100

    wip

---
+++ components/section.tsx
@@ -0,0 +1,26 @@
+import { motion } from 'framer-motion';
+import { chakra, shouldForwardProp } from '@chakra-ui/react';
+
+const StyledDiv = chakra(
+  motion.div, {
+    shouldForwardProp: prop => shouldForwardProp(prop) || prop === 'transition',
+  }
+);
+
+const Section: React.FC<SectionProps> = ({ children, delay = 0 }) => (
+  <StyledDiv
+    initial={{ y: 10, opacity: 0 }}
+    animate={{ y: 0, opacity: 1 }}
+    transition={{ duration: 0.8, delay }}
+    mb={6}
+  >
+    {children}
+  </StyledDiv>
+);
+
+export default Section;
+
+type SectionProps = {
+  delay: number;
+  children: React.ReactNode;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
 import { FunctionComponent } from 'react';
-import { Container, Flex, Box, Heading } from '@chakra-ui/react';
+import { Container, Flex, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react';
+
+import Section from '../components/section';
 
 const Page: FunctionComponent = () => {
   return (
     <Container>
-      <Flex borderRadius='lg' bg='brown' p={8} align='center' justify='center'>
+      <Flex
+        borderRadius='lg'
+        bg={useColorModeValue('brown', '#bada55')}
+        p={8}
+        align='center'
+        justify='center'
+        color={useColorModeValue('white', 'black')}
+      >
         Hellou!
       </Flex>
       <Box display={{ md: 'flex' }}>
@@ -14,7 +23,31 @@ const Page: FunctionComponent = () => {
           </Heading>
           <p>Javascript Developer</p>
         </Box>
+        <Flex
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ base: 6 }}
+          justifyContent='center'
+        >
+          <Image
+            borderColor='whiteAlpha.800'
+            alt='Profile image'
+            borderWidth={2}
+            borderStart='solid'
+            maxWidth='100px'
+            display='inline-block'
+            borderRadius='full'
+            src='/images/profile.jpg'
+          />
+        </Flex>
       </Box>
+
+      <Section delay={0.1}>
+        <Heading as='h3' variant='section-title'>
+          Work
+        </Heading>
+        <p>asldkjlk jaljdlkasjdl jjdjasd</p>
+      </Section>
     </Container>
   );
 };
